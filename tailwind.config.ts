076fcc98afbc36b261b3a7ef8fc8eb6943d7a787
mateoghidini1998 <mateghidini123@gmail.com>
2024-07-29 12:31:58 -0300

    Add color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      textColor: {
+        "lightblue-primary": "#17AAE1",
+        "gree-primary": "#82CC6D",
+        "red-primary": "#EC5647",
+        "purple-primary": "#6104EE",
+        "purple-secondary": "#B33ADB",
+        "orange-primar": "#E84729",
+        "mustard": "#F19424",
+        "grey-primary": "#888"
+      }
     },
-  },
+  }, 
   plugins: [],
 };
 export default config;
